37b87df7d874788d924e278231b14ca8e4abd433
filipjovicccc <filipjovicccc@gmail.com>
2022-11-05 21:27:36 +0100

    update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,49 +5,57 @@ import { HiOutlineRefresh } from "react-icons/hi";
 function Counter() {
   const [characterCounter, setCharacterCounter] = useState(0);
   const [wordCounter, setWordCounter] = useState(0);
-  const [input, setInput] = useState(" ");
-  const [inputLock, setInputLock] = useState(false);
-  const inputChangeHandler = (e) => {
-    if (e.target.value) {
+  const inputRef = useRef();
+  const inputChangeHandler = (event) => {
+    if (event.target.value) {
       setCharacterCounter((count) => count + 1);
     }
-    setInput(e.target.value);
-    const expmp = input.split(" ");
-
-    setWordCounter(expmp.length);
+ const inputChange = inputRef.current.value.split(" ");
+    const filteredInput = inputChange.filter((item) => item !== "");
+ setWordCounter(filteredInput.length);
   };
-  const style = { color: "#545454", fontSize: "2.5em" };
 
   const clearRefresherHandler = (e) => {
     setCharacterCounter(0);
     setWordCounter(0);
-    setInput("");
+    inputRef.current.value = " ";
   };
+  const style = { color: "#545454", fontSize: "2.5em" };
+  const deleteWithBackspace = (event) => {
+    if (event.key === "Backspace") {
+      setCharacterCounter(0);
 
+      setWordCounter(0);
+      inputRef.current.value = "";
+    }
+  };
   return (
     <div className="wrapper">
       <div className="header">
         <h1>Word counter</h1>
 
-        <HiOutlineRefresh style={style} />
-        <button onClick={clearRefresherHandler}></button>
+        <button className="btn" onClick={clearRefresherHandler}>
+          <HiOutlineRefresh style={style} />
+        </button>
       </div>
 
       <div className="main">
         <textarea
+          onKeyUp={deleteWithBackspace}
           onChange={inputChangeHandler}
           placeholder="Enter text here"
           id="textarea"
+          ref={inputRef}
         ></textarea>
 
         <div className="section">
           <div className="section-wrapp">
-            <h3>Words:</h3>
+            <h6>Words:</h6>
             <p>{wordCounter}</p>
           </div>
 
           <div className="section-wrapp">
-            <h3>Characters:</h3>
+            <h6>Characters:</h6>
             <p>{characterCounter}</p>
           </div>
         </div>
